a49b2b38ea03f95c7f0859944a1fbaa6d9f2d3d8
portellii <119754985+alecdportelli@users.noreply.github.com>
2024-11-16 18:14:39 -0500

    Added state vector processing to image server

--- Networking/ImageServer.py
+++ Networking/ImageServer.py
@@ -2,7 +2,8 @@ from .Server import Server
 
 from PIL import Image
 from io import BytesIO
-import os
+import json
+import os 
 
 
 class ImageServer( Server ):
@@ -15,36 +16,64 @@ class ImageServer( Server ):
     def Recieve(self):
         """ Start recieving the data """
         while True:
-            # Receive the image size (4 bytes)
-            sizeBytes = self.conn.recv(4)
-            if not sizeBytes:
+            # Receive a header byte to determine the type of incoming data (e.g., image or state vector)
+            header = self.conn.recv(1)
+            if not header:
                 break
 
-            # Convert size bytes to integer (image size)
-            imgSize = int.from_bytes(sizeBytes, byteorder='little')
-            print(f"Receiving image of size: {imgSize} bytes")
+            # Header value
+            dataType = header[0]
 
-            # Receive the image data in chunks
-            imgData = b""
-            while len(imgData) < imgSize:
-                packet = self.conn.recv(4096)  # Receive in chunks
-                if not packet:
-                    break
-                imgData += packet
-
-            if len(imgData) == imgSize:
-                # Convert the received byte data to an image
-                image = Image.open(BytesIO(imgData))
-                # image.show() # TODO: Can process more here
-
-                if self.collectPath is not None:
-                    imgPath = os.path.join(self.collectPath, f'img_{self.i}.png')
-                    image.save(imgPath)
-                    self.i += 1
-            else:
-                print("Image data is incomplete!")
+            if dataType == 1:  # Image data
+                self.ReceiveImage()
+            elif dataType == 2:  # State vector data
+                self.ReceiveStateVector()
         
         # Close the connection when done
         print("Connection closed.")
         self.conn.close()
         self.Stop()
+
+
+    def ReceiveImage(self):
+        """ Receive image data """
+        sizeBytes = self.conn.recv(4)
+        imgSize = int.from_bytes(sizeBytes, byteorder='little')
+        imgData = b""
+        
+        while len(imgData) < imgSize:
+            packet = self.conn.recv(4096)
+            if not packet:
+                break
+            imgData += packet
+
+        if len(imgData) == imgSize:
+            # Process image data
+            image = Image.open(BytesIO(imgData))
+            if self.collectPath is not None:
+                img_path = os.path.join(self.collectPath, f"image_{self.i}.png")
+                image.save(img_path)
+                print(f"Image saved to: {img_path}")
+                self.i += 1
+            else:
+                print("Image data is incomplete!")
+
+
+    def ReceiveStateVector(self):
+        """ Receive state vector data """
+        sizeBytes = self.conn.recv(4)
+        stateSize = int.from_bytes(sizeBytes, byteorder='little')
+        stateData = b""
+
+        while len(stateData) < stateSize:
+            packet = self.conn.recv(4096)
+            if not packet:
+                break
+            stateData += packet
+
+        if len(stateData) == stateSize:
+            stateJson = stateData.decode("utf-8")
+            # Deserialize JSON to state vector
+            stateVector = json.loads(stateJson)
+            # Process the state vector as needed
+            print(stateVector)
